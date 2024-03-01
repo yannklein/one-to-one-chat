9b1a51d90819c575437398da2ebf3c3f601035d0
Yann Klein <yann.klein@me.com>
2024-03-02 00:36:11 +0900

    refine query to find common chatroom in chatroom create

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -6,18 +6,13 @@ class ChatroomsController < ApplicationController
 
   def create
     @other_user = User.find(params[:user_id])
-    other_user_chatroom_ids = Chatroom.joins(:users).where(users: @other_user).pluck(:id)
-    current_user_chatroom_ids = Chatroom.joins(:users).where(users: current_user).pluck(:id)
-    common_chatroom_id = (other_user_chatroom_ids & current_user_chatroom_ids).first
+    @chatroom = Chatroom.joins(:users).where(users: [@other_user, current_user]).group(:id).having("COUNT(users.id) = 2").first
 
     # if no common chatroom ids, create a new chatroom and chatroomusers
-    if common_chatroom_id.nil?
+    if @chatroom.nil?
       @chatroom = Chatroom.new(name: "#{@other_user.nickname}-#{current_user.nickname}")
       ChatroomUser.create(user: current_user, chatroom: @chatroom)
       ChatroomUser.create(user: @other_user, chatroom: @chatroom)
-    #else use the exisiting chatroom
-    else
-      @chatroom = Chatroom.find(common_chatroom_id)
     end
     redirect_to chatroom_path(@chatroom)
   end
