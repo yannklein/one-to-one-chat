99d34d8ffb768b13d6023857b98494d49c81be33
Yann Klein <yann.klein@me.com>
2024-03-02 14:16:20 +0900

    chaneg Chatroom new to create and add SQL queries exlpanations

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -8,9 +8,18 @@ class ChatroomsController < ApplicationController
     @other_user = User.find(params[:user_id])
     @chatroom = Chatroom.joins(:users).where(users: [@other_user, current_user]).group(:id).having("COUNT(users.id) = 2").first
 
+    # SQL query produced:
+    # select c.id, c.name, COUNT(u.id)
+    # from chatrooms c 
+    # join chatroom_users cu on c.id = cu.chatroom_id 
+    # join users u on cu.user_id = u.id
+    # where u.id in (12, 14)
+    # group by c.id
+    # having COUNT(u.id) = 2
+
     # if no common chatroom ids, create a new chatroom and chatroomusers
     if @chatroom.nil?
-      @chatroom = Chatroom.new(name: "#{@other_user.nickname}-#{current_user.nickname}")
+      @chatroom = Chatroom.create(name: "#{@other_user.nickname}-#{current_user.nickname}")
       ChatroomUser.create(user: current_user, chatroom: @chatroom)
       ChatroomUser.create(user: @other_user, chatroom: @chatroom)
     end
